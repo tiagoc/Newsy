0da41731b893e955ef08547d10ed6ce84c95ea30
maria- <mj@MacBook-Air-de-Maria.local>
2014-03-11 02:09:01 +0000

    Very small CSS changes to the main news' page.

--- pages/news.php
+++ pages/news.php
@@ -42,7 +42,7 @@
             <div class="row">
                 <div class="large-6 columns">
                     <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa.</p>
-                    <p>Boudin aliqua adipisicing rump corned beef. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami.</p>
+                    <p>Boudin aliqua adipisicing rump corned beef. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami. </p>
                 </div>
                 <div class="large-6 columns">
                     <img src="http://placehold.it/400x240&text=[img]" />
@@ -53,7 +53,8 @@
 
             <p>Pork drumstick turkey fugiat. Tri-tip elit turducken pork chop in. Swine short ribs meatball irure bacon nulla pork belly cupidatat meatloaf cow. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami.</p>
 
-            <p class="text-right">1 comment <br/> Categories: Politics, Food </p>
+            <div id="category">Categories</div> <p>Politics, Food </p>
+            <p class="text-right">0 comments</p> 
         </article>
 
         <hr />
@@ -77,7 +78,8 @@
 
             <p>Pork drumstick turkey fugiat. Tri-tip elit turducken pork chop in. Swine short ribs meatball irure bacon nulla pork belly cupidatat meatloaf cow. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami.</p>
 
-            <p class="text-right">0 comments <br/> Categories: Politics, Food </p>
+            <div id="category">Categories</div> <p>Politics, Food </p>
+            <p class="text-right">0 comments</p> 
         </article>
 
     </div>
