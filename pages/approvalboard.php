483256aa9d4b1f6b29a28c449621bb9ff7f6679e
Cristiano Rodrigues <cristianoalexandre@gmail.com>
2014-03-13 03:18:25 +0000

    Set the width of approval board to 100%.

--- pages/approvalboard.php
+++ pages/approvalboard.php
@@ -3,135 +3,135 @@
 <div class="row">
     <div class="small-12 columns">
         <h1>Approval Board</h1>
-             
+
         <div class="approvaltab">
-        <dl class="tabs" data-tab>
-          <dd class="active"><a href="#panel2-1">News pending approval</a></dd>
-          <dd><a href="#panel2-2">Approved News</a></dd>
-          <dd><a href="#panel2-3">Reproved News</a></dd>
-        </dl>
-        <div class="tabs-content">
-          <div class="content active" id="panel2-1">
-              
-            <!--  Pending news tab -->
+            <dl class="tabs" data-tab>
+                <dd class="active"><a href="#panel2-1">News pending approval</a></dd>
+                <dd><a href="#panel2-2">Approved News</a></dd>
+                <dd><a href="#panel2-3">Reproved News</a></dd>
+            </dl>
+            <div class="tabs-content">
+                <div class="content active" id="panel2-1">
 
-                <div class = "pendingnews">
+                    <!--  Pending news tab -->
 
-                <table>
-                  <thead>
-                    <tr>
-                      <th width="150">Title</th>
-                      <th width="150">Creator</th>
-                      <th width="150">Publish</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td><a>Real stuff happened</a></td>
-                      <td>Jonh Doe</td>
-                      <td><a href="#" class="tiny button radious">Publish</a></td>
-                    </tr>
-                    <tr>
-                      <td><a>New news</a></td>
-                      <td>Emma Derpson</td>
-                      <td><a href="#" class="tiny button radious">Publish</a></td>
-                    </tr>
-                      <tr>
-                      <td><a>Who let the dogs out?</a></td>
-                      <td>Judite Almeida</td>
-                      <td><a href="#" class="tiny button radious">Publish</a></td>
-                    </tr>
-                  </tbody>
-                </table>
+                    <div class = "pendingnews">
 
-                </div>
-              
-              <!--  Pending news tab end -->
-              
-          </div>
-          <div class="content" id="panel2-2">
-              
-              
-              <!--  Approved news tab -->
+                        <table style="width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th width="150">Title</th>
+                                    <th width="150">Creator</th>
+                                    <th width="150">Publish</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a>Real stuff happened</a></td>
+                                    <td>Jonh Doe</td>
+                                    <td><a href="#" class="tiny button radious">Publish</a></td>
+                                </tr>
+                                <tr>
+                                    <td><a>New news</a></td>
+                                    <td>Emma Derpson</td>
+                                    <td><a href="#" class="tiny button radious">Publish</a></td>
+                                </tr>
+                                <tr>
+                                    <td><a>Who let the dogs out?</a></td>
+                                    <td>Judite Almeida</td>
+                                    <td><a href="#" class="tiny button radious">Publish</a></td>
+                                </tr>
+                            </tbody>
+                        </table>
 
-                <div class = "approvednews">
+                    </div>
 
-                <table>
-                  <thead>
-                    <tr>
-                      <th width="150">Title</th>
-                      <th width="150">Creator</th>
-                      <th width="150">Publish</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td><a>The countdown</a></td>
-                      <td>Steve Nojob</td>
-                      <td><a href="#" class="tiny button alert radious">Unpublish</a></td>
-                    </tr>
-                    <tr>
-                      <td><a>Important things</a></td>
-                      <td>Catherine York</td>
-                      <td><a href="#" class="tiny button alert radious">Unpublish</a></td>
-                    </tr>
-                      <tr>
-                      <td><a>Great questions</a></td>
-                      <td>Francesco Silva</td>
-                      <td><a href="#" class="tiny button alert radious">Unpublish</a></td>
-                    </tr>
-                  </tbody>
-                </table>
+                    <!--  Pending news tab end -->
 
                 </div>
-              
-              <!--  Approved news tab end -->
-              
-              
-          </div>
-          <div class="content" id="panel2-3">
-            
-              
-              <!--  Reproved news tab -->
+                <div class="content" id="panel2-2">
 
-                <div class = "approvednews">
 
-                <table>
-                  <thead>
-                    <tr>
-                      <th width="150">Title</th>
-                      <th width="150">Creator</th>
-                      <th width="150">Publish</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      
-                    </tr>
-                    <tr>
-                     
-                    </tr>
-                    <tr>
-                  
-                    </tr>
-                  </tbody>
-                </table>
+                    <!--  Approved news tab -->
+
+                    <div class = "approvednews">
+
+                        <table style="width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th width="150">Title</th>
+                                    <th width="150">Creator</th>
+                                    <th width="150">Publish</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a>The countdown</a></td>
+                                    <td>Steve Nojob</td>
+                                    <td><a href="#" class="tiny button alert radious">Unpublish</a></td>
+                                </tr>
+                                <tr>
+                                    <td><a>Important things</a></td>
+                                    <td>Catherine York</td>
+                                    <td><a href="#" class="tiny button alert radious">Unpublish</a></td>
+                                </tr>
+                                <tr>
+                                    <td><a>Great questions</a></td>
+                                    <td>Francesco Silva</td>
+                                    <td><a href="#" class="tiny button alert radious">Unpublish</a></td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+
+                    <!--  Approved news tab end -->
+
 
                 </div>
-              
-              <!--  Reproved news tab end -->
-              
-              
-          </div>
-        </div>
-        
-        
-        
-        
+                <div class="content" id="panel2-3">
 
-       
 
+                    <!--  Reproved news tab -->
+
+                    <div class = "approvednews">
+
+                        <table style="width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th width="150">Title</th>
+                                    <th width="150">Creator</th>
+                                    <th width="150">Publish</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+
+                                </tr>
+                                <tr>
+
+                                </tr>
+                                <tr>
+
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+
+                    <!--  Reproved news tab end -->
+
+
+                </div>
             </div>
+
+
+
+
+
+
+
+        </div>
 
 
 
@@ -141,4 +141,5 @@
     </div>
 </div>
 
-<?php include './footer.php'; ?>
+<?php
+         include './footer.php'; ?>
