c1e89a48a43f285d22917930c4dfa7abef1ff880
Tiago Cruzeiro <tiago.c.1991@gmail.com>
2014-03-14 05:02:04 +0000

    Added search form to sidebar

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -1,6 +1,11 @@
 <!-- Sidebar -->
 <aside class="large-3 columns">
     <div class="panel">
+    
+        <input type="text" placeholder="Search">
+<!--        <a href="#" class="button expand">Search</a>-->
+        
+       
         <h5 class="text-center">Categories</h5>
         <ul class="side-nav">
             <li class="text-center"><a href="#">Politics</a></li>
