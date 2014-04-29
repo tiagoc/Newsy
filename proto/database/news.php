7956af3b4f2f5c307646efa0b9124c3a8893d75a
Cristiano Rodrigues <cristianoalexandre@gmail.com>
2014-04-29 23:51:20 +0100

    Started working on the mynews.php page, and adding the necessary functions on the database/news.php script.

--- proto/database/news.php
+++ proto/database/news.php
@@ -28,18 +28,76 @@ function getArticle($article_id) {
                             WHERE id = ?;
     ");
     $stmt->execute(array($article_id));
+    
+    return $stmt->fetch();
 }
 
 function submitNews($title, $synopsis, $body) {
     global $conn;
     $stmt = $conn->prepare("INSERT INTO news
                             VALUES(DEFAULT, ?, ?, ?, DEFAULT, DEFAULT, 'submitted', DEFAULT ,$_SESSION[id]);");
-    $stmt->execute(array($title, $synopsis, $body));    
+    return $stmt->execute(array($title, $synopsis, $body));        
 }
 
 function saveDraft($title, $synopsis, $body) {
     global $conn;
     $stmt = $conn->prepare("INSERT INTO news
                             VALUES(DEFAULT, ?, ?, ?, DEFAULT, DEFAULT, 'draft', DEFAULT ,$_SESSION[id]);");
-    $stmt->execute(array($title, $synopsis, $body));    
+    return $stmt->execute(array($title, $synopsis, $body));    
+}
+
+function getAllJournalistNews($user_id) {
+    global $conn;
+    $stmt = $conn->prepare("SELECT id, title, state FROM news
+                            WHERE journalist_id = ?;");
+    $stmt->execute(array($user_id));
+    
+    return $stmt->fetchAll();        
+}
+
+function getNewsDates($news_id) {
+    global $conn;
+    $publishdates = getPublishDates($news_id);
+    $rejectdates = getRejectDates($news_id);
+    $submissiondates = getSubmissionDates($news_id);
+    $draftdates = getDraftDates($news_id);
+    
+    $dates = array("publishdates"=>$publishdates,"rejectdates"=>$rejectdates,"submissiondates"=>$submissiondates,"draftdates"=>$draftdates);
+    return $dates;
+}
+
+function getPublishDates($news_id) {
+    global $conn;
+ 
+    $stmt = $conn->prepare("SELECT to_char(published_at,'HH24:MI:SS') FROM publishes WHERE news_id = ?;");
+    $stmt->execute(array($news_id));
+    
+    return $stmt->fetchAll();
+}
+
+function getSubmissionDates($news_id) {
+    global $conn;
+ 
+    $stmt = $conn->prepare("SELECT to_char(submitted_at,'HH24:MI:SS') FROM submissions WHERE news_id = ?;");
+    $stmt->execute(array($news_id));
+    
+    return $stmt->fetchAll();
+}
+
+function getDraftDates($news_id) {
+    global $conn;
+ 
+    $stmt = $conn->prepare("SELECT to_char(saved_at,'HH24:MI:SS') FROM drafts WHERE news_id = ?;");
+    $stmt->execute(array($news_id));
+    
+    return $stmt->fetchAll();
+}
+
+function getRejectDates($news_id) {
+    global $conn;
+ 
+    $stmt = $conn->prepare("SELECT to_char(rejected_at,'HH24:MI:SS') FROM drafts WHERE news_id = ?;");
+    $stmt->execute(array($news_id));
+    
+    return $stmt->fetchAll();
 }
--- proto/pages/users/mynews.php
+++ proto/pages/users/mynews.php
@@ -1,5 +1,9 @@
 <?php
 include_once '../../config/init.php';
+include_once $BASE_DIR."database/news.php";
+
+$news = getAllJournalistNews($_SESSION['id']);
+$smarty->assign("news",$news);
 
 $smarty->display("users/mynews.tpl");
 ?>
